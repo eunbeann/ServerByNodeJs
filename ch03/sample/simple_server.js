49ced588a6ff91c774233eb1e60062133f9a634f
Eunbeann <ds5bean23@gmail.com>
2023-08-01 15:36:40 +0900

    [ ch03 ] 리스닝 이벤트 리스너

--- ch03/sample/simple_server.js
+++ ch03/sample/simple_server.js
@@ -1,12 +1,15 @@
 const http = require("http");
 
-http
+const server = http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
     res.write("<h1> Node.js로 서버 만들기 </h1> ");
     res.end("<p> 3장 http 모듈 공부 중 </p>");
-    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
   })
-  .listen(8080, () => {
-    console.log("8080포트에서 연결중");
-  });
+  .listen(8080);
+
+// Listening Event Listener
+
+server.on("listening", () => {
+  console.log("8080포트에서 서버 연결 중..");
+});
