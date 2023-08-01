1ecbf9359147545bd3a4f4c724d1bed39b13d6bc
Eunbeann <ds5bean23@gmail.com>
2023-08-01 15:33:47 +0900

    [ ch03 ] 웹 페이지의 요청에 대한 응답

--- ch03/sample/simple_server.js
+++ ch03/sample/simple_server.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
 http
-  .createServer((req, res) => {})
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.write("<h1> Node.js로 서버 만들기 </h1> ");
+    res.end("<p> 3장 http 모듈 공부 중 </p>");
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  })
   .listen(8080, () => {
     console.log("8080포트에서 연결중");
   });
