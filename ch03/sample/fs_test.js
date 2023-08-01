2c0beaab66d9e0554bdac37a9773569e3a6f9007
Eunbeann <ds5bean23@gmail.com>
2023-08-01 15:46:19 +0900

    [ ch03 ] 오타 변경 log -> error

--- ch03/sample/fs_test.js
+++ ch03/sample/fs_test.js
@@ -8,7 +8,7 @@ http
       const f = await fs.readFile("./fs_test.html");
       res.writeHead(200, { "Content-Type": "text.html; charset=utf-8" });
     } catch (error) {
-      console.log(err);
+      console.error(err);
       res.writeHead(500, { "Content-Type": " text.html; charset=utf-8 " });
       // 500이면 서버 오류 발생
       res.end(err.message); // 오류 메시지와 함게 요청 종료
