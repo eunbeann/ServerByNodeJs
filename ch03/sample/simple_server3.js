38e0d543fc198ebf54a2c56b40ce3f96694cb478
Eunbeann <ds5bean23@gmail.com>
2023-08-04 00:08:12 +0900

    [ ch03 ] simple_server03

---
+++ ch03/sample/simple_server3.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+http
+  .createServer((req, res) => {
+    console.log(req);
+    console.log(res);
+  })
+
+  .listen(8080, () => {
+    console.log("8080 포트에서 연결");
+  });
