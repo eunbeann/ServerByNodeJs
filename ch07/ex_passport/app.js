c62ccfc2f48074a4f7c73b704d18ae9e210f6ac5
Eunbeann <ds5bean23@gmail.com>
2023-08-04 19:50:36 +0900

    [ ch07 ] 포트 설정

--- ch07/ex_passport/app.js
+++ ch07/ex_passport/app.js
@@ -4,3 +4,8 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const passport = require("passport");
 const Localstrategy = require("passport-local").strategy;
+
+const app = express();
+
+//포트 설정
+app.set("port", process.env.PORT || 8080);
