78f1d0f732d2e1671bd88f4c1066f730eb281989
Eunbeann <ds5bean23@gmail.com>
2023-08-04 19:49:44 +0900

    [ch07] import

---
+++ ch07/ex_passport/app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("passport");
+const Localstrategy = require("passport-local").strategy;
