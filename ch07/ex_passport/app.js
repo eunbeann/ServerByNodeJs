02162fe645153407f300f2080f8e3e7c21f764d4
Eunbeann <ds5bean23@gmail.com>
2023-08-05 23:30:28 +0900

    [ ch07 ] inner passport

--- ch07/ex_passport/app.js
+++ ch07/ex_passport/app.js
@@ -32,3 +32,28 @@ app.use(
     },
   })
 );
+
+// passport 미들 웨어
+
+app.use(passport.initialize()); //passport 초기화
+app.use(passport.session()); // passport session 전용
+
+// 세션 처리  = 로그인에 성공했을 경우 딱 한 번 호출되어 사용자의 식별자를 session에 저장
+passport.deserializeUser(function (id, done) {
+  console.log("deserializeUser", id);
+  done(null, fakeUser); //req.user에 전달
+});
+
+passport.use(
+  new Localstrategy(function (username, password, done) {
+    if (username === fakeUser.username) {
+      if (password === fakeUser.username) {
+        return done(null, fakeUser);
+      } else {
+        return done(null, false, { message: "password incorrect" });
+      }
+    } else {
+      return done(null, false, { message: " username incorrect" });
+    }
+  })
+);
