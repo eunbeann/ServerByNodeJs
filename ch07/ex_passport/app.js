8c926e74811d82a8b1c7fbddf10b819fb2390ca2
Eunbeann <ds5bean23@gmail.com>
2023-08-10 20:17:35 +0900

    [ ch07 ] 새션 처리

--- ch07/ex_passport/app.js
+++ ch07/ex_passport/app.js
@@ -34,11 +34,16 @@ app.use(
 );
 
 // passport 미들 웨어
-
 app.use(passport.initialize()); //passport 초기화
 app.use(passport.session()); // passport session 전용
 
 // 세션 처리  = 로그인에 성공했을 경우 딱 한 번 호출되어 사용자의 식별자를 session에 저장
+passport.serializeUser(function (user, done) {
+  console.log("serializeUser", user);
+  done(null, user.username); //req.user에 전달
+});
+
+// 세션 처리 - 로그인 후 페이지 방문마다 사용자의 실제 데이터 주입
 passport.deserializeUser(function (id, done) {
   console.log("deserializeUser", id);
   done(null, fakeUser); //req.user에 전달
