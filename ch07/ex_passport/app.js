d19127345ada0f1e941b3eed84ce59a83dcac30f
Eunbeann <ds5bean23@gmail.com>
2023-08-10 19:56:46 +0900

    [ ch07 ] 오타 수정

--- ch07/ex_passport/app.js
+++ ch07/ex_passport/app.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const passport = require("passport");
-const Localstrategy = require("passport-local").strategy;
+const Localstrategy = require("passport-local").Strategy;
 
 const app = express();
 
